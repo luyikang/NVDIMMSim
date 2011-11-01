d2c2bbea010221eabde50f3cdbf6c782eab9aa57
Paul <phantomofmind@gmail.com>
2011-11-01 16:27:57 -0400

    fixed problem with the logger in the gc and p8p cases where a full system log was not being generated

--- src/P8PGCLogger.cpp
+++ src/P8PGCLogger.cpp
@@ -158,22 +158,15 @@ void P8PGCLogger::save(uint64_t cycle, uint epoch)
 	     }
 	}
 
-        if(RUNTIME_WRITE)
+	string command_str = "test -e "+LOG_DIR+" || mkdir "+LOG_DIR;
+	const char * command = command_str.c_str();
+	int sys_done = system(command);
+	if (sys_done != 0)
 	{
-	    savefile.open(LOG_DIR+"NVDIMM.log", ios_base::out | ios_base::app);
+	    WARNING("Something might have gone wrong when nvdimm attempted to makes its log directory");
 	}
-	else
-	{
-	    string command_str = "test -e "+LOG_DIR+" || mkdir "+LOG_DIR;
-	    const char * command = command_str.c_str();
-	    int sys_done = system(command);
-	    if (sys_done != 0)
-	    {
-		WARNING("Something might have gone wrong when nvdimm attempted to makes its log directory");
-	    }
-	    savefile.open(LOG_DIR+"NVDIMM.log", ios_base::out | ios_base::trunc);
-	    savefile<<"NVDIMM Log \n";
-	}
+	savefile.open(LOG_DIR+"NVDIMM.log", ios_base::out | ios_base::trunc);
+	savefile<<"NVDIMM Log \n";
 
 	if (!savefile) 
 	{
