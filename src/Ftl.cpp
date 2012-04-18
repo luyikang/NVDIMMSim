fa144ccaf2981c009e1e10013b72401b7b45f79c
Paul <phantomofmind@gmail.com>
2012-04-18 15:53:44 -0400

    double checking to make sure that its the used line by commenting out the fast write line

--- src/Ftl.cpp
+++ src/Ftl.cpp
@@ -483,12 +483,10 @@ void Ftl::handle_disk_read(bool gc)
 	}
 	used_page_count++;
 	addressMap[vAddr] = pAddr;
-
-	cout << used_page_count << "\n";
 	
 	// quick write the page
-	ChannelPacket *tempPacket = Ftl::translate(WRITE, vAddr, pAddr);
-	controller->writeToPackage(tempPacket);
+	//ChannelPacket *tempPacket = Ftl::translate(WRITE, vAddr, pAddr);
+	//controller->writeToPackage(tempPacket);
 	
 	//=============================================================================
 	// the read part
@@ -742,8 +740,6 @@ void Ftl::write_success(uint64_t block, uint64_t page, uint64_t vAddr, uint64_t 
     // Set the used bit for this page to true.
     used[block][page] = true;
     used_page_count++;
-
-    cout << used_page_count << "\n";
 	
     // Pop the transaction from the transaction queue.
     popFront(WRITE);
