18534753eefdc2e52f1a1d35ed2a8b7cc184c5ae
Paul <phantomofmind@gmail.com>
2012-05-07 15:19:52 -0400

    for debugging purposes I've disabled the recieving while sending when the buffer is nearly full functionality in the buffer code

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -154,8 +154,8 @@ bool Buffer::isFull(SenderType t, ChannelPacketType bt, uint64_t die)
     if(t == CONTROLLER)
     {
 	if(IN_BUFFER_SIZE == 0 || (bt == 5 && inDataSize[die] <= (IN_BUFFER_SIZE-(NV_PAGE_SIZE*8192))) || 
-	   (bt != 5 && inDataSize[die] <= (IN_BUFFER_SIZE-COMMAND_LENGTH)) ||
-	   (inDataSize[die] <= (IN_BUFFER_SIZE-CHANNEL_WIDTH) && waiting[die] == false))
+	   (bt != 5 && inDataSize[die] <= (IN_BUFFER_SIZE-COMMAND_LENGTH))) // ||
+	   //(inDataSize[die] <= (IN_BUFFER_SIZE-CHANNEL_WIDTH) && waiting[die] == false))
 	{
 	    return false;
 	}
